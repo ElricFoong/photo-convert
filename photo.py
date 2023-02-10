a8353794979fe8ed212519aa205c2ce929945d5a
ElricFoong <elricfoong@gmail.com>
2023-02-10 20:34:53 +1100

    add os.path.join

--- photo.py
+++ photo.py
@@ -2,7 +2,8 @@ from PIL import Image
 import os
 
 for i in os.listdir('original'):
-    if i.endswith('jpg'):
-        image_file = Image.open('original/' + i)
+    if i.endswith('jpg'):  # or ('original/' + i )
+        image_file = Image.open(os.path.join('original', i))
         image_file = image_file.convert('L')
-        image_file.save('result/' + i[:-4] + '_grey.jpg')
+        image_file.save(os.path.join('result', i[:-4] + '_grey.jpg'))
+        #   ('result/' + i[:-4] + '_grey.jpg)
